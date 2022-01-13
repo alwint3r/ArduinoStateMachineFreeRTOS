8f3d2ac4d96f8bb0d3c4b04054c4911938962a0b
Alwin Arrasyid <alwin.ridd@gmail.com>
2022-01-13 22:17:05 +0700

    breaking: everything is templated

--- src/FreeRTOSMachine.h
+++ src/FreeRTOSMachine.h
@@ -5,20 +5,58 @@
 #include <Task.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
-
-class FreeRTOSMachine: public StateMachine, public Task
+template <typename T, class C>
+class FreeRTOSMachine: public StateMachine<T, C>, public Task
 {
 public:
-    FreeRTOSMachine();
-    virtual ~FreeRTOSMachine();
+    FreeRTOSMachine()
+    {
+        _queue = xQueueCreate(5, sizeof(StateEvent<T, C>));
+    }
 
-    void run(void *pvParameters) override;
+    virtual ~FreeRTOSMachine()
+    {
+        vQueueDelete(_queue);
+    }
+
+    void run(void *pvParameters) override
+    {
+        StateEvent<T, C> event;
+        if (_currentState != nullptr)
+        {
+            _currentState->enter();
+        }
+
+        while (1)
+        {
+            if (xQueueReceive(_queue, &event, portMAX_DELAY) == pdPASS)
+            {
+                processEvent(&event);
+        
+                vTaskDelay(pdMS_TO_TICKS(100));
+            }
+        }
+    }
 
 protected:
-    void dispatchEvent(const StateEvent &event) override;
-    void dispatchEventFromISR(const StateEvent &event) override;
+    void dispatchEvent(const StateEvent<T, C> &event) override
+    {
+        Serial.printf("Received event %d\n", event.id);
+          xQueueSend(_queue, &event, 0);
+    }
+
+    void dispatchEventFromISR(const StateEvent<T, C> &event) override
+    {
+        xQueueSendFromISR(_queue, &event, 0);
+    }
 
 protected:
+    using StateMap = std::map<std::string, StateClass<T, C> *>;
+    using StatePair = std::pair<std::string, StateClass<T, C> *>;
+
+    StateMap *_stateMap;
+    StateClass<T, C> *_currentState = nullptr;
+
     QueueHandle_t _queue;
 };
 
